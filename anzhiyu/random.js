a8eeffae634236f7d82d1adb70660e7a2f46780f
Lkaijl <JJ2006727@outlook.com>
2025-03-15 14:54:50 +0800

    Site updated: 2025-03-15 14:54:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/14/hello-world/"];function toRandomPost(){
+var posts=["2025/03/14/hello-world/","2025/03/15/GHCTF/ghctf/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
