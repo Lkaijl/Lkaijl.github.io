45840b8af1bba1e9568df748754f927fba2bcec6
Lkaijl <JJ2006727@outlook.com>
2025-04-07 21:10:49 +0800

    Site updated: 2025-04-07 21:10:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/24/Hook/IAT_Hook/","2025/03/20/Hook/inline_hook/","2025/03/24/Linux/Linux基础知识/","2025/03/31/CTFshow/Ctfshow_php特性/","2025/03/15/GHCTF/ghctf/"];function toRandomPost(){
+var posts=["2025/03/31/CTFshow/Ctfshow_php特性/","2025/03/20/Hook/inline_hook/","2025/03/24/Hook/IAT_Hook/","2025/03/15/GHCTF/ghctf/","2025/04/07/kali/kali安装docker/","2025/03/24/Linux/Linux基础知识/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
