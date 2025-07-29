94871a9833a33bbcfd5e2ba49944da2f8762bfdb
Lkaijl <JJ2006727@outlook.com>
2025-07-29 17:22:24 +0800

    Site updated: 2025-07-29 17:22:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/20/Hook/inline_hook/","2025/05/29/CTFshow/ctfshow_ssti/","2025/03/24/Hook/IAT_Hook/","2025/04/30/CTFshow/反序列化知识/","2025/03/15/GHCTF/ghctf/","2025/04/22/CTFshow/反序列化/","2025/04/07/kali/kali安装docker/","2025/07/07/算法/算法/","2025/03/24/Linux/Linux基础知识/","2025/03/31/CTFshow/Ctfshow_php特性/","2025/04/21/CTFshow/upload/"];function toRandomPost(){
+var posts=["2025/05/29/CTFshow/ctfshow_ssti/","2025/04/21/CTFshow/upload/","2025/04/22/CTFshow/反序列化/","2025/04/30/CTFshow/反序列化知识/","2025/03/31/CTFshow/Ctfshow_php特性/","2025/07/07/算法/算法/","2025/03/15/GHCTF/ghctf/","2025/03/24/Hook/IAT_Hook/","2025/03/24/Linux/Linux基础知识/","2025/04/07/kali/kali安装docker/","2025/03/20/Hook/inline_hook/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
